b9b0cd641cefb2ad2daa88f8b76860eda3a5960e
nabeek <nabeek@gmail.com>
2020-07-06 18:42:19 -0600

    Removed webpack to ensure app offline capability

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,33 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/assets/css/styles.css",
-  "/dist/bundle.js",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches
-      .open(PRECACHE)
+      .open(STATIC_CACHE)
       .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+      .then(() => self.skipWaiting())
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", event => {
-  const currentCaches = [PRECACHE, RUNTIME];
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
   event.waitUntil(
     caches
       .keys()
       .then(cacheNames => {
+        // return array of cache names that are old to delete
         return cacheNames.filter(
           cacheName => !currentCaches.includes(cacheName)
         );
@@ -43,21 +44,46 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/images")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
       })
     );
+    return;
   }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
 });
--- server.js
+++ server.js
@@ -1,27 +1,29 @@
+// Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+// Middleware
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve public folder
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
-// routes
+// Routing
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
